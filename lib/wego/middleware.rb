7d28d9db2213e658be93d3e1087ede7683e0c42e
Aarlo Stone Fish <aarlo@li213-214.members.linode.com>
2012-07-16 12:45:32 -0700

    remove deprecated caching include

--- lib/wego/middleware.rb
+++ lib/wego/middleware.rb
@@ -1,6 +1,5 @@
 module Wego
   module Middleware
-    autoload :Caching, 'wego/middleware/caching'
     autoload :Http,    'wego/middleware/http'
     autoload :Logging, 'wego/middleware/logging'
   end
